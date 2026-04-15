7f4af5c2862f1bd9e08deb74ffac8523885eb80a
Prachi Gupta <prachig150904@gmail.com>
2026-04-15 22:04:01 +0530

    Implemented Authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Auth from './pages/Auth'
 
+export const ServerUrl = "http://localhost:8000"
 function App() {
   return (
-    <div>
-      
-    </div>
+   <Routes>
+     <Route path='/' element=<Home/> />
+     <Route path='/auth' element=<Auth/> />
+   </Routes>
   )
 }
 
